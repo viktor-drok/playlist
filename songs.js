d66590c6696c323ead518514de7be8a814c32bfa
viktor-drok <vityadrok@gmail.com>
2022-08-22 20:42:47 +0300

    add song text

--- songs.js
+++ songs.js
@@ -2,42 +2,382 @@ const playList = [
     {
         author: "LED ZEPPELIN",
         song: "STAIRWAY TO HEAVEN",
-        text: 'asdfdsfsdfgsdgnsdgmdngmsdn,mdsng,msdngdm,sgn'
+        text: `[Intro]
+
+        [Verse 1]
+There's a lady who's sure all that glitters is gold
+And she's buying a stairway to Heaven
+When she gets there she knows, if the stores are all closed
+With a word she can get what she came for
+Ooh, ooh, and she's buying a stairway to Heaven
+There's a sign on the wall, but she wants to be sure
+'Cause you know sometimes words have two meanings
+In a tree by the brook, there's a songbird who sings
+Sometimes all of our thoughts are misgiven
+[Chorus]
+Ooh, it makes me wonder
+Ooh, makes me wonder
+
+[Verse 2]
+There's a feeling I get when I look to the West
+And my spirit is crying for leaving
+In my thoughts I have seen rings of smoke through the trees
+And the voices of those who stand looking
+
+[Chorus]
+Ooh, it makes me wonder
+Ooh, really makes me wonder
+
+[Verse 3]
+And it's whispered that soon if we all call the tune
+Then the piper will lead us to reason
+And a new day will dawn for those who stand long
+And the forests will echo with laughter
+
+[Chorus]
+Oh - oh - oh - oh - woahhh
+
+[Verse 4]
+If there's a bustle in your hedgerow, don't be alarmed now
+It's just a spring clean for the May queen
+Yes, there are two paths you can go by, but in the long run
+There's still time to change the road you're on
+Related Songs
+ALIEN SUPERSTAR
+Beyoncé
+Hotel California
+Eagles
+All the Things I Hate About You
+Huddy
+[Chorus]
+And it makes me wonder
+Ohh, woah
+
+[Verse 5]
+Your head is humming, and it won't go, in case you don't know
+The piper's calling you to join him
+Dear lady, can you hear the wind blow ? And did you know
+Your stairway lies on the whispering wind ?
+
+    [Guitar Solo]
+
+    [Bridge]
+And as we wind on down the road
+Our shadows taller than our soul
+There walks a lady we all know
+Who shines white light and wants to show
+How everything still turns to gold
+And if you listen very hard
+The tune will come to you at last
+When all are one, and one is all
+To be a rock and not to roll
+
+[Outro]
+And she's buying a stairway to Heaven`
     },
     {
         author: "QUEEN",
         song: "BOHEMIAN RHAPSODY",
-        text: 'treuytiyuoiop   rfdyhufgjh    sfhy'
+        text: `Is this the real life?
+Is this just fantasy?
+Caught in a landside,
+No escape from reality
+Open your eyes,
+Look up to the skies and see,
+I'm just a poor boy, I need no sympathy,
+Because I'm easy come, easy go,
+Little high, little low,
+Any way the wind blows doesn't really matter to
+Me, to me
+Mamaaa,
+Just killed a man,
+Put a gun against his head, pulled my trigger,
+Now he's dead
+Mamaaa, life had just begun,
+But now I've gone and thrown it all away
+Mama, oooh,
+Didn't mean to make you cry,
+If I'm not back again this time tomorrow,
+Carry on, carry on as if nothing really matters
+Too late, my time has come,
+Sends shivers down my spine, body's aching all
+The time
+Goodbye, everybody, I've got to go,
+Gotta leave you all behind and face the truth
+Mama, oooh
+I don't want to die,
+I sometimes wish I'd never been born at all.
+I see a little silhouetto of a man,
+Scaramouch, Scaramouch, will you do the Fandango!
+Thunderbolts and lightning, very, very frightening me
+Galileo, Galileo
+Galileo, Galileo
+Galileo, Figaro - magnificoo
+I'm just a poor boy nobody loves me
+He's just a poor boy from a poor family,
+Spare him his life from this monstrosity
+Easy come, easy go, will you let me go
+Bismillah! No, we will not let you go
+(Let him go!) Bismillah! We will not let you go
+(Let him go!) Bismillah! We will not let you go
+(Let me go) Will not let you go
+(Let me go)(Never) Never let you go
+(Let me go) (Never) let you go (Let me go) Ah
+No, no, no, no, no, no, no
+Oh mama mia, mama mia, mama mia, let me go
+Beelzebub has a devil put aside for me, for me,
+For meee
+So you think you can stop me and spit in my eye
+So you think you can love me and leave me to die
+Oh, baby, can't do this to me, baby,
+Just gotta get out, just gotta get right outta here
+Nothing really matters, Anyone can see,
+Nothing really matters,
+Nothing really matters to me
+Any way the wind blows...`
     },
     {
         author: "LYNYRD SKYNYRD",
         song: "FREE BIRD",
-        text: 'llddlll llll llll'
+        text: `If I leave here tomorrow
+Would you still remember me?
+For I must be traveling on now
+'Cause there's too many places I've got to see
+But if I stay here with you, girl
+Things just couldn't be the same
+'Cause I'm as free as a bird now
+And this bird you cannot change
+Oh, oh, oh, oh
+And the bird you cannot change
+And this bird, you cannot change
+Lord knows, I can't change
+Bye-bye, baby, it's been sweet love, yeah, yeah
+Though this feelin' I can't change
+Please don't take it so badly
+'Cause Lord knows, I'm to blame
+If I stay here with you, girl
+Things just couldn't be the same
+'Cause I'm as free as a bird now
+And this bird you cannot change
+Oh, oh, oh, oh
+And the bird you cannot change
+And this bird, you cannot change
+Lord knows, I can't change
+Lord help me, I can't change
+Lord, I can't change
+Won't you fly high, free bird, yeah`
     },
     {
         author: "DEEP PURPLE",
         song: "SMOKE ON THE WATER",
-        text: 'dddd dddd dddd'
+        text: `We all came out to Montreux
+On the Lake Geneva shoreline
+To make records with a mobile
+We didn't have much time
+But Frank Zappa and the Mothers
+Were at the best place around
+But some stupid with a flare gun
+Burned the place to the ground
+
+Smoke on the water, a fire in the sky
+Smoke on the water
+
+They burned down the gambling house
+It died with an awful sound
+Funky Claude was running in and out
+Pulling kids out the ground
+When it all was over
+We had to find another place
+But Swiss time was running out
+It seemed that we would lose the race
+
+Smoke on the water, a fire in the sky
+Smoke on the water
+
+We ended up at the Grand Hotel
+It was empty cold and bare
+But with the Rolling-truck-Stones-thing just outside, ha
+Making our music there
+With a few red lights, a few old beds, huh
+We made a place to sweat
+No matter what we get out of this, ha
+I know, I know we'll never forget
+
+Smoke on the water, a fire in the sky
+Smoke on the water`
     },
     {
         author: "JIMI HENDRIX",
         song: "ALL ALONG THE WATCHTOWER",
-        text: 'fff fff  ffff'
+        text: `There must be some kind of way outta here
+Said the joker to the thief
+There's too much confusion
+I can't get no relief
+Business men, they drink my wine
+Plowmen dig my earth
+None will level on the line
+Nobody offered his word
+Hey, hey
+No reason to get excited
+The thief, he kindly spoke
+There are many here among us
+Who feel that life is but a joke
+But, uh, but you and I, we've been through that
+And this is not our fate
+So let us stop talkin' falsely now
+The hour's getting late, hey
+Hey
+All along the watchtower
+Princes kept the view
+While all the women came and went
+Barefoot servants, too
+Well, uh, outside in the cold distance
+A wildcat did growl
+Two riders were approaching
+And the wind began to howl, hey
+All along the watchtower
+All along the watchtower`
     },
     {
         author: "AC/DC",
         song: "BACK IN BLACK",
-        text: 'ggggg ggggg ggggg'
+        text: `Back in black
+I hit the sack
+I've been too long, I'm glad to be back
+Yes, I'm let loose
+From the noose
+That's kept me hanging about
+I've been looking at the sky
+'Cause it's gettin' me high
+Forget the hearse 'cause I never die
+I got nine lives
+Cat's eyes
+Abusin' every one of them and running wild
+'Cause I'm back
+Yes, I'm back
+Well, I'm back
+Yes, I'm back
+Well, I'm back, back
+Well, I'm back in black
+Yes, I'm back in black
+Back in the back
+Of a Cadillac
+Number one with a bullet, I'm a power pack
+Yes, I'm in a bang
+With a gang
+They've got to catch me if they want me to hang
+'Cause I'm back on the track
+And I'm beatin' the flack
+Nobody's gonna get me on another rap
+So look at me now
+I'm just makin' my play
+Don't try to push your luck, just get out of my way
+'Cause I'm back
+Yes, I'm back
+Well, I'm back
+Yes, I'm back
+Well, I'm back, back
+Well, I'm back in black
+Yes, I'm back in black
+Well, I'm back, yes, I'm back
+Well, I'm back, yes, I'm back
+Well, I'm back, back
+Well, I'm back in black
+Yes, I'm back in black
+Oh, yeah
+Oh, yeah
+Take my love
+Oh, yeah, yeah
+Oh, yeah, yeah, ooh, yeah
+Well, I'm back (I'm back)
+Back (I'm back)
+Back (I'm back)
+Back (I'm back)
+Back (I'm back)
+Back
+Back in black
+Yes, I'm back in black
+I've hit the sack`
     },
     {
         author: "QUEEN",
         song: "WE WILL ROCK YOU",
-        text: 'hhhhhh hhhhhh '
+        text: `Buddy, you're a boy, make a big noise
+Playing in the street, gonna be a big man someday
+You got mud on your face, you big disgrace
+Kicking your can all over the place, singin'
+We will, we will rock you
+We will, we will rock you
+Buddy, you're a young man, hard man
+Shouting in the street, gonna take on the world someday
+You got blood on your face, you big disgrace
+Waving your banner all over the place
+We will, we will rock you, sing it!
+We will, we will rock you, yeah
+Buddy, you're an old man, poor man
+Pleading with your eyes, gonna get you some peace someday
+You got mud on your face, big disgrace
+Somebody better put you back into your place, do it!
+We will, we will rock you, yeah, yeah, come on
+We will, we will rock you, alright, louder!
+We will, we will rock you, one more time
+We will, we will rock you
+Yeah`
     },
     {
         author: "METALLICA",
         song: "ENTER SANDMAN",
-        text: 'dfdgdgdgd xfsdfxv dfdg'
+        text: `Say your prayers, little one
+Don't forget, my son
+To include everyone
+Tuck you in, warm within
+Keep you free from sin
+'Til the sandman, he comes
+Sleep with one eye open
+Gripping your pillow tight
+Exit light
+Enter night
+Take my hand
+We're off to never-never land
+Somethings wrong, shut the light
+Heavy thoughts tonight
+And they aren't of Snow White
+Dreams of war, dreams of liars
+Dreams of dragon's fire
+And of things that will bite, yeah
+Sleep with one eye open
+Gripping your pillow tight
+Exit light
+Enter night
+Take my hand
+We're off to never-never land
+Now I lay me down to sleep
+Now I lay me down to sleep
+I pray the Lord my soul to keep
+I pray the Lord my soul to keep
+If I die before I wake
+If I die before I wake
+I pray the Lord my soul to take
+I pray the Lord my soul to take
+Hush, little baby, don't say a word
+And never mind that noise you heard
+It's just the beasts under your bed
+In your closet, in your head
+Exit light
+Enter night
+Grain of sand
+Exit light
+Enter night
+Take my hand
+We're off to never-never land, yeah
+Uh
+Yeah, yeah
+Yo, oh
+We're off to never-never land
+Take my hand
+We're off to never-never land
+Take my hand
+We're off to never-never land`
     }
 ];
 
